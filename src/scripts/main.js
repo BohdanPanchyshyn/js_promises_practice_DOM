4fc09198b74b3966c5d7dbdbeda21745e7828c93
SageUA <panchyshyn33@gmail.com>
2025-12-30 18:02:53 +0200

    Solution #2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,8 +12,10 @@ const successHandler = (text) => {
   body.appendChild(div);
 };
 
-const errorHandler = (text) => {
+const errorHandler = (err) => {
   const div = document.createElement('div');
+
+  const text = err instanceof Error ? err.message : String(err);
 
   div.dataset.qa = 'notification';
   div.classList.add('error');
@@ -23,29 +25,39 @@ const errorHandler = (text) => {
 };
 
 const firstPromise = new Promise((resolve, reject) => {
-  const leftClick = document.addEventListener('click', () => {
-    resolve('First promise was resolved');
-  });
+  document.addEventListener(
+    'click',
+    () => {
+      resolve('First promise was resolved');
+    },
+    { once: true },
+  );
 
   setTimeout(() => {
-    if (!leftClick) {
-      reject(new Error('First promise was rejected'));
-    }
+    reject(new Error('First promise was rejected'));
   }, 3000);
 });
 
 firstPromise.then(successHandler).catch(errorHandler);
 
 const secondPromise = new Promise((resolve) => {
-  document.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
+  document.addEventListener(
+    'contextmenu',
+    (e) => {
+      e.preventDefault();
 
-    resolve('Second promise was resolved');
-  });
+      resolve('Second promise was resolved');
+    },
+    { once: true },
+  );
 
-  document.addEventListener('click', (e) => {
-    resolve('Second promise was resolved');
-  });
+  document.addEventListener(
+    'click',
+    (e) => {
+      resolve('Second promise was resolved');
+    },
+    { once: true },
+  );
 });
 
 secondPromise.then(successHandler);
@@ -54,21 +66,29 @@ const thirdPromise = new Promise((resolve, reject) => {
   let leftClick = false;
   let rightClick = false;
 
-  document.addEventListener('click', () => {
-    leftClick = true;
+  document.addEventListener(
+    'click',
+    () => {
+      leftClick = true;
 
-    if (leftClick && rightClick) {
-      resolve('Third promise was resolved');
-    }
-  });
+      if (leftClick && rightClick) {
+        resolve('Third promise was resolved');
+      }
+    },
+    { once: true },
+  );
 
-  document.addEventListener('contextmenu', () => {
-    rightClick = true;
+  document.addEventListener(
+    'contextmenu',
+    () => {
+      rightClick = true;
 
-    if (leftClick && rightClick) {
-      resolve('Third promise was resolved');
-    }
-  });
+      if (leftClick && rightClick) {
+        resolve('Third promise was resolved');
+      }
+    },
+    { once: true },
+  );
 });
 
 thirdPromise.then(successHandler);
